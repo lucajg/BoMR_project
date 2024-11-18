574c55d5c5ccaed761617a0abdefaf3c18c6a2e4
Victor Dubois <victor.dubois@outlook.com>
2024-11-18 17:07:04 +0100

    circle tracking and color recognition

--- vision/vision_main.py
+++ vision/vision_main.py
@@ -3,6 +3,27 @@ import cv2
 import sys
 from functions import *
 
+# Define a class for circles
+class Circle:
+    def __init__(self, center, radius, color):
+        self.center = center  # Center position as a tuple (x, y)
+        self.radius = radius  # Radius as a float
+        self.color = color    # Color as a tuple (B, G, R)
+
+    def is_in_range(self, ref_color):
+        ref_color = np.array(ref_color)
+        dmax = 150 # max eculidian distance for the color to be considered red
+        d = color_distance(self.color, ref_color)
+        return (d <= dmax)
+
+    def __repr__(self):
+        return f"Circle(center={self.center}, radius={self.radius}, color={self.color})"
+
+# Function to compute euclidian distance between two colors
+def color_distance(color1, color2):
+    color1 = np.array(color1)
+    color2 = np.array(color2)
+    return np.linalg.norm(color1 - color2)
 
 # Open Camera
 s = 0
@@ -21,15 +42,85 @@ else:
 win_name = "Camera"
 cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
 
+# Define global var
+traj = np.zeros(2)
+green_ref = (50,200,0)
+red_ref = (0,30,170)
+
+
 alive = True
 while alive:
+    # Initialize empty arrays
+    circles = np.array([])
+    green_circles = np.array([])
+    red_circles = np.array([])
+
+    # Read frame from the camera
     has_frame, frame = source.read()
     if not has_frame:
         break
     
-    result = cv2.Canny(frame, 40, 100)
+    frame = cv2.flip(frame, 1)
+
+    # Preprocessing for Hough transform
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.medianBlur(gray, 5)
+
+    # Compute circle Hough transform
+    rows = gray.shape[0]
+    circles_hough = cv2.HoughCircles(gray,cv2.HOUGH_GRADIENT,1,rows/8,param1=100,param2=30,minRadius=5,maxRadius=50)
     
-    cv2.imshow(win_name, result)
+    if circles_hough is not None:
+        circles_hough = np.uint16(np.around(circles_hough))       
+        print(len(circles_hough[0,:]))
+        for i,circle in enumerate(circles_hough[0, :]):
+            center = (circle[0], circle[1])
+            radius = circle[2]
+            # circle mean color
+            mask = np.zeros(frame.shape[:2], dtype=np.uint8)
+            cv2.circle(mask, center, radius, 255, -1)
+            color = cv2.mean(frame, mask=mask)
+            circles = np.append(circles, Circle(center, radius, color[:3]))
+            print(circles[i], color_distance(circles[i].color, green_ref),color_distance(circles[i].color, red_ref))
+            
+    
+    # Keep track of green and red circles
+    for circle in circles:
+        # Check if the circle is green
+        if circle.is_in_range(green_ref):
+            green_circles = np.append(green_circles, circle)
+        # Check if the circle is red
+        if circle.is_in_range(red_ref):
+            red_circles = np.append(red_circles, circle)
+
+    # Extract circles closest to the ref colors
+    green_circle = None
+    red_circle = None
+    min_distance = float('inf')  # Initialize with a large value
+    # Green
+    for circle in green_circles:
+        distance = color_distance(circle.color, green_ref)
+        
+        if distance < min_distance:
+            min_distance = distance
+            green_circle = circle
+    # Red
+    min_distance = float('inf')  # Initialize with a large value
+    for circle in red_circles:
+        distance = color_distance(circle.color, red_ref)
+        
+        if distance < min_distance:
+            min_distance = distance
+            red_circle = circle
+
+    # Draw red and green circles
+    if green_circle is not None:
+        cv2.circle(frame, green_circle.center, green_circle.radius, green_ref, 3)
+    if red_circle is not None:
+        cv2.circle(frame, red_circle.center, red_circle.radius, red_ref, 3)
+
+
+    cv2.imshow(win_name, frame)
     
     key = cv2.waitKey(1)
     if key == ord("Q") or key == ord("q") or key == 27:
